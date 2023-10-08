d9276f4cc7f061a89d336a0ffc252a3ba06edcde
AlexSpeal <alexspeal@gmail.com>
2023-10-08 20:58:24 +0300

    task0

--- src/main/java/edu/hw1/task0.java
+++ src/main/java/edu/hw1/task0.java
@@ -1,8 +1,9 @@
 package edu.hw1;
+
 import java.util.logging.Logger;
+
 public class task0 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Logger logger = Logger.getLogger(task0.class.getName());
         logger.info("Привет, мир");
     }
