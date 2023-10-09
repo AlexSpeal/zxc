97386a8cb577171bf5743ad7cf40ca0be6d1e6dd
AlexSpeal <alexspeal@gmail.com>
2023-10-09 11:38:37 +0300

    task3

---
+++ src/test/java/edu/hw1/Task4Test.java
@@ -0,0 +1,26 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task4Test {
+    @Test
+    void fixString() {
+        String actual = Task4.fixString("hTsii  s aimex dpus rtni.g");
+        String expected = "This is a mixed up string.";
+        assertEquals(expected, actual);
+
+        actual = Task4.fixString("123456");
+        expected = "214365";
+        assertEquals(expected, actual);
+
+        actual = Task4.fixString("");
+        expected = "";
+        assertEquals(expected, actual);
+
+        actual = Task4.fixString(" ");
+        expected = " ";
+        assertEquals(expected, actual);
+    }
+
+}
