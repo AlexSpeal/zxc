93e9454fb8d2e3b8a886445cf35c089b18d98c88
AlexSpeal <alexspeal@gmail.com>
2023-10-15 16:50:10 +0300

    ds

--- src/main/java/edu/hw2/Expr.java
+++ src/main/java/edu/hw2/Expr.java
@@ -1,6 +1,6 @@
 package edu.hw2;
 
-public sealed interface Expr permits Expr.Addition, Expr.Constant, Expr.Negate, Expr.Multiplication,Expr.Exponent {
+public sealed interface Expr permits Expr.Addition, Expr.Constant, Expr.Negate, Expr.Multiplication, Expr.Exponent {
     double evaluate();
 
     public record Constant(double value) implements Expr {
@@ -17,20 +17,19 @@ public sealed interface Expr permits Expr.Addition, Expr.Constant, Expr.Negate, 
 
     public record Negate(Expr value) implements Expr {
         public double evaluate() {
-            return (-1) * value.evaluate();
+            return value.evaluate() == 0 ? 0 : (-1) * value.evaluate();
         }
     }
 
-    public record Multiplication(Expr first,Expr second) implements Expr {
+    public record Multiplication(Expr first, Expr second) implements Expr {
         public double evaluate() {
-            return first.evaluate()*second.evaluate();
+            return first.evaluate() * second.evaluate();
         }
     }
 
-
-    public record Exponent(Expr value,int pow) implements Expr {
+    public record Exponent(Expr value, int pow) implements Expr {
         public double evaluate() {
-            return Math.pow(value.evaluate(),pow);
+            return (value.evaluate() == 0 && pow < 0) ? Double.POSITIVE_INFINITY : Math.pow(value.evaluate(), pow);
         }
     }
 
---
+++ src/test/java/edu/hw2/task1Test.java
@@ -0,0 +1,92 @@
+package edu.hw2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class task1Test {
+    @Test
+    @DisplayName("метод Constant")
+    void test1() {
+        double actual = new Expr.Constant(-1).evaluate();
+        double expected = -1;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Constant(10).evaluate();
+        expected = 10;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("метод Addition")
+    void test2() {
+        double actual = new Expr.Addition(new Expr.Constant(5), new Expr.Constant(5)).evaluate();
+        double expected = 10;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Addition(new Expr.Constant(-5), new Expr.Constant(5)).evaluate();
+        expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("метод Negate")
+    void test3() {
+        double actual = new Expr.Negate(new Expr.Constant(5)).evaluate();
+        double expected = -5;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Negate(new Expr.Constant(-4)).evaluate();
+        expected = 4;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Negate(new Expr.Constant(0)).evaluate();
+        expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("метод Multiplication")
+    void test4() {
+        double actual = new Expr.Multiplication(new Expr.Constant(5), new Expr.Constant(5)).evaluate();
+        double expected = 25;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Multiplication(new Expr.Constant(0), new Expr.Constant(5)).evaluate();
+        expected = 0;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Multiplication(new Expr.Constant(-1), new Expr.Constant(5)).evaluate();
+        expected = -5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("метод Exponent")
+    void test5() {
+        double actual = new Expr.Exponent(new Expr.Constant(5), 2).evaluate();
+        double expected = 25;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Exponent(new Expr.Constant(2), 0).evaluate();
+        expected = 1;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Exponent(new Expr.Constant(2), -1).evaluate();
+        expected = 0.5;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Exponent(new Expr.Constant(0), -1).evaluate();
+        expected = Double.POSITIVE_INFINITY;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Exponent(new Expr.Constant(0), 5).evaluate();
+        expected = 0;
+        assertEquals(expected, actual);
+
+        actual = new Expr.Exponent(new Expr.Constant(0), 0).evaluate();
+        expected = 1;
+        assertEquals(expected, actual);
+    }
+}
+
