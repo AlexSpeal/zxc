0f798b59d34f08bbbc92d82ef88129fe169f5b09
AlexSpeal <alexspeal@gmail.com>
2023-10-29 19:26:26 +0300

    hw3(finally)

---
+++ src/main/java/edu/hw3/Task8/BackwardIterator.java
@@ -0,0 +1,34 @@
+package edu.hw3.Task8;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class BackwardIterator<T> implements Iterator<T> {
+
+    private final Collection<T> collection;
+    private int index;
+
+    protected BackwardIterator(Collection<T> collection) {
+        if (collection != null) {
+            this.collection = collection;
+            index = collection.size() - 1;
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index >= 0;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            return null;
+        }
+        T[] elem = (T[]) new Object[collection.size()];
+        collection.toArray(elem);
+        return elem[index--];
+    }
+}
